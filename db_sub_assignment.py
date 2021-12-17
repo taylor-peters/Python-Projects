2429096ee30bf2be8d4ed3aeee55f2cd7b74b4ac
Taylor Peters <taylordanielpeters@gmail.com>
2021-12-16 20:33:41 -0800

    Added comments to code

--- db_sub_assignment.py
+++ db_sub_assignment.py
@@ -1,7 +1,9 @@
 import sqlite3
 
+# Connects to database
 conn = sqlite3.connect('submission.db')
 
+# Creates 'tbl_items' table with two columns
 with conn:
     cur = conn.cursor()
     cur.execute('CREATE TABLE IF NOT EXISTS tbl_items( \
@@ -17,9 +19,10 @@ fileList = ('information.docs', 'Hello.txt', 'myImage.png',
 conn = sqlite3.connect('submission.db')
 
 for file in fileList:
-    if file.endswith('.txt'):
+    if file.endswith('.txt'):  # Checks ending portion of file name for ".txt"
          with conn:
             cur = conn.cursor()
+            # Adds the index of the for loop as the value of the column
             cur.execute("INSERT INTO tbl_items(file_name) VALUES (?) ", (file,))
             print(file)
     conn.commit()
