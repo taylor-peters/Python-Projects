79d463504d8e11afe05b9afd184b7b7f1bc8df42
Taylor Peters <taylordanielpeters@gmail.com>
2021-12-16 21:17:15 -0800

    Ineritance Submission

---
+++ test1.py
@@ -0,0 +1,14 @@
+class Vehicle:
+    transMethod = ""
+    fuel = ""
+    weight = 0
+    passengers = True
+    capacity = 0
+
+class Car(Vehicle):
+    model = ""
+    color = ""
+
+class Airplane(Vehicle):
+    airline = ""
+    reg_number = ""
